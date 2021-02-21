7b9d2bbd57926df0b9b10d2879f50ba0c85b309c
Michela Guzzo <michela.guzzo@gmail.com>
2021-02-21 23:12:05 +0100

    frontend config

---
+++ poll_app/frontend/src/components/JoinRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JoinRoom extends Component{
+    render(){
+        return(
+            <div>
+                <p>This is the join room page</p>
+            </div>
+        )
+    }
+}
+
+export default JoinRoom
